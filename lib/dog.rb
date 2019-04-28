66a0bc0b062302a31caa6664f6354545d6de1a34
Mary Selig <seligmar@gmail.com>
2019-04-28 21:54:18 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+Give a Dog a Name
+You'll be teaching Dog about their names through two methods, #name, and #name= that read and write to a corresponding instance variable @name.
+
+Give a Dog a Breed
+You'll be teaching Dog about their breed through two methods, #breed, and #breed= that read and write to a corresponding instance variable @breed.
+
+class Dog
+  def name
+  end 
+  def name=
+  end 
+  @name
+end
